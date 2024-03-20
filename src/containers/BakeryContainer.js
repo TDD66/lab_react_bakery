cefc1cbd721fd4ffc820a1d7bdd38d30267aab01
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-20 14:47:32 +0000

    Created cakes in bakerycontainer

---
+++ src/containers/BakeryContainer.js
@@ -0,0 +1,35 @@
+const  BakeryContainer = () => {
+    
+    victoriaSponge = {
+        cakeName: "Victoria Sponge",
+        ingredients: ["eggs", "butter", "sugar", "self-raising flour", "baking powder", "milk"],
+        price: 5,
+        rating: 5
+    }
+
+    teaLoaf = {
+        cakeName: "Tea Loaf",
+        ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
+        price: 2,
+        rating: 3
+    }
+
+    carrotCake = {
+        cakeName: "Carrot Cake",
+        ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
+        price: 8,
+        rating: 5
+    }
+
+
+
+    const cakes = [victoriaSponge, teaLoaf, carrotCake];
+    
+    return (
+        <>
+
+        </>
+    );
+}
+ 
+export default BakeryContainer;
