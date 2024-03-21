6cfc267984b18157e89034f372b5491bed5b45a7
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-21 10:33:42 +0000

    BakeryContainer now uses working JSON payload

--- src/containers/BakeryContainer.js
+++ src/containers/BakeryContainer.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Cake from "../components/Cake";
+import BakeryItemsPayload from './BakeryItemsPayload.json';
 
 import './BakeryContainer.css';
 
@@ -10,29 +11,8 @@ const  BakeryContainer = () => {
     const sellCake = (price) => {
         setTotalValueSold(totalValueSold + price);
     }
-    
-    const victoriaSponge = {
-        cakeName: "Victoria Sponge",
-        ingredients: ["eggs", "butter", "sugar", "self-raising flour", "baking powder", "milk"],
-        price: 5,
-        rating: 5
-    }
 
-    const teaLoaf = {
-        cakeName: "Tea Loaf",
-        ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
-        price: 2,
-        rating: 3
-    }
-
-    const carrotCake = {
-        cakeName: "Carrot Cake",
-        ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
-        price: 5,
-        rating: 5
-    }
-
-    const [cakes, setCakes] = useState([victoriaSponge, teaLoaf, carrotCake]);
+    const [cakes, setCakes] = useState(BakeryItemsPayload);
 
     const displayCakes = () => {
         return cakes.map((cake, index) => <Cake key={index} cake={cake} sellCake={sellCake}/>);
